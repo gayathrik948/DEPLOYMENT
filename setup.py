13b1373ea0ac3461cbaea299b57b840f6bea3639
gayathrik948 <gayathrik948@gmail.com>
2022-09-13 12:59:23 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="DEPLOYMENT", 
+    author="gayathrik948", 
+    packages=find_packages(),
+    license="MIT"
+)
